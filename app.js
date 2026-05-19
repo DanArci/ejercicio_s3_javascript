2edc0414ec333a185cb75c80bcc9b7bf87c18869
Daniel arci <danielarciniegaspua@gmail.com>
2026-05-19 15:56:47 -0500

    x

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 let lista = document.getElementById('lista')
 let storage = localStorage
+let notas = []
+
+function cargar2() {
+    for (const key in storage) {
+        if (!Object.hasOwn(storage, key)) continue;
+        let contenido = JSON.parse(storage[key]);
+        let descripcion = contenido[0]
+        let status = contenido[1]        
+        let id = key.replace(/ /g,"_")
+        notas.push([])
+    }
+}
+
+
+
 
 function agregar(titulo, descripcion) {
     localStorage.setItem(titulo,JSON.stringify(descripcion))
+    cargar()
 }
 
-console.log(storage);
-
-
-function content() {
+function cargar() {
+    lista.innerHTML = ""
     for (const key in storage) {
         if (!Object.hasOwn(storage, key)) continue;
         let contenido = JSON.parse(storage[key]);
@@ -23,5 +37,6 @@ function content() {
     }
 }
 
-content()
+cargar()
+// agregar("Tercera prueba de notas",["Esto se debe agregar y actualizar enseguida en el html", true])
 
