3e5b07935ba9ac9fdb520d69face79199f934066
Daniel arci <danielarciniegaspua@gmail.com>
2026-05-19 15:33:45 -0500

    Html y parte de la logica

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-let Tareas = {
-    TareaNombre : {
-        descripcion : "",
-        status : true
+let lista = document.getElementById('lista')
+let storage = localStorage
+
+function agregar(titulo, descripcion) {
+    localStorage.setItem(titulo,JSON.stringify(descripcion))
+}
+
+console.log(storage);
+
+
+function content() {
+    for (const key in storage) {
+        if (!Object.hasOwn(storage, key)) continue;
+        let contenido = JSON.parse(storage[key]);
+        let descripcion = contenido[0]
+        let status = contenido[1]        
+        let id = key.replace(/ /g,"_")
+        lista.innerHTML += `
+            <li id=${id} status=${status}>
+                <h2>${key}</h2>
+                <p>${descripcion}</p>
+            </li>`
     }
 }
 
-localStorage.setItem(
-    Riwi,
-    Tareas
-)
+content()
 
-let TareaNombre = document.getElementById("input_form")
